532a56901837ca8475bdea1651d465a27246cfb0
@codeApostle1 <joelmtn7@gmail.com>
2025-12-31 23:29:13 +0100

    marks removed

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -8,11 +8,11 @@ const BookInfo = () => {
       </p>
 
       <ul style={{ maxWidth: "600px", margin: "40px auto", lineHeight: "1.8", color: "#334155" }}>
-        <li>✔ Clear learning roadmap</li>
-        <li>✔ Beginner-friendly explanations</li>
-        <li>✔ Curated video resources</li>
-        <li>✔ Practical project guidance</li>
-        <li>✔ No unnecessary theory</li>
+        <li> Clear learning roadmap</li>
+        <li> Beginner-friendly explanations</li>
+        <li> Curated video resources</li>
+        <li> Practical project guidance</li>
+        <li> No unnecessary theory</li>
       </ul>
     </section>
   )
